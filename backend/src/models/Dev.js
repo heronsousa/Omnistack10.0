383c16948f070cad960763e29e672e1e030b8287
Heron Rodrigues <heronrs1@gmail.com>
2020-01-15 20:26:53 -0300

    signing up dev on database

--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const DevSchema = new mongoose.Schema({
     name: String,
-    github: String,
+    github_username: String,
     bio: String,
     avatar_url: String,
     techs: [String]
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,29 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.post('/devs', (req, res) => {
-    const { name } = req.body;
-    return res.json({asd : 'asd'});
+routes.post('/devs', async (req, res) => {
+    const { github_username, techs } = req.body;
+    
+    const response = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    // Se name n existir, recebe o login por padrão
+    const {avatar_url, name = login, bio} = response.data;
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    // Short syntax, quando a propriedade e o valor tem a mesma denominação
+    const dev = await Dev.create({
+        github_username,
+        name,
+        bio,
+        avatar_url,
+        techs: techsArray
+    });
+
+    return res.json({ dev });
 });
 
 module.exports = routes;
