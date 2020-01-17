a45379ed09866defae801e291139d535b96f2681
Heron Rodrigues <heronrs1@gmail.com>
2020-01-17 18:54:49 -0300

    signing up dev to app api

--- web/src/App.js
+++ web/src/App.js
@@ -4,19 +4,24 @@ import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+import api from './services/api';
 
 function App() {
-
+  
+  const [ devs, setDevs] = useState([]);
   const [ latitude, setLatitude] = useState('');
   const [ longitude, setLongitude] = useState('');
+  const [ github_username, setGithubUsername] = useState('');
+  const [ techs, setTechs] = useState('');
 
+  
   // É executada toda vez que uma informação for alterada
   // useEffect("função que será executada", "vetor de variaveis monitoradas")
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
-
+        
         setLatitude(latitude);
         setLongitude(longitude);
       },
@@ -26,33 +31,83 @@ function App() {
       {
         timeout: 30000
       }
-    )
-  }, []);
+      )
+    }, []);
+    
+    useEffect(() => {
+      async function loadDevs(){
+        const response = await api.get('/devs');
+        
+        setDevs(response.data.devs);
+      }
+      
+      loadDevs();
+    }, []);
+    
+  async function handleSubmit(e){
+    // Impede que, ao submeter o formulário, seja redirecionado para outra página
+    e.preventDefault();
+    
+    const response = await api.post('/devs',{
+      github_username,
+      techs,
+      longitude,
+      latitude
+    });
+
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-block">
             <label htmlFor='github_username'>Usuário do Github</label>
-            <input name='github_username' id='github_username' required/>
+            <input 
+              name='github_username' 
+              id='github_username' 
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor='techs'>Tecnologias</label>
-            <input name='techs' id='techs' required/>
+            <input 
+              name='techs' 
+              id='techs' 
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor='latitude'>Latitude</label>
-              <input type='number' name='latitude' id='latitude' required value={latitude}/>
+              <input 
+                type='number' 
+                name='latitude' 
+                id='latitude' 
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor='longitude'>Longitude</label>
-              <input type='number' name='longitude' id='longitude' required value ={longitude}/>
+              <input 
+                type='number' 
+                name='longitude' 
+                id='longitude' 
+                required 
+                value = {longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
           
@@ -62,53 +117,19 @@ function App() {
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/37983059?s=460&v=4" alt="Heron Rodrigues"/>
-              <div className="user-info">
-                <strong>Heron Rodrigues</strong>
-                <span>ReactJS, Node.js</span>
-              </div>
-            </header>
-            <p>Graduando do curso Engenharia de Software na Universidade de Brasília - UnB</p>
-            <a href="https://github.com/heronsousa">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/37983059?s=460&v=4" alt="Heron Rodrigues"/>
-              <div className="user-info">
-                <strong>Heron Rodrigues</strong>
-                <span>ReactJS, Node.js</span>
-              </div>
-            </header>
-            <p>Graduando do curso Engenharia de Software na Universidade de Brasília - UnB</p>
-            <a href="https://github.com/heronsousa">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/37983059?s=460&v=4" alt="Heron Rodrigues"/>
-              <div className="user-info">
-                <strong>Heron Rodrigues</strong>
-                <span>ReactJS, Node.js</span>
-              </div>
-            </header>
-            <p>Graduando do curso Engenharia de Software na Universidade de Brasília - UnB</p>
-            <a href="https://github.com/heronsousa">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/37983059?s=460&v=4" alt="Heron Rodrigues"/>
-              <div className="user-info">
-                <strong>Heron Rodrigues</strong>
-                <span>ReactJS, Node.js</span>
-              </div>
-            </header>
-            <p>Graduando do curso Engenharia de Software na Universidade de Brasília - UnB</p>
-            <a href="https://github.com/heronsousa">Acessar perfil no Github</a>
-          </li>
+          {devs.map( dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name}/>
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p> 
+              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
