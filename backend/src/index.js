7a388acb2822d95d8c378f8319bd3b43cd53f89b
Heron Rodrigues <heronrs1@gmail.com>
2020-01-21 20:37:01 -0300

    adding socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
+const { setupWebSocket } = require('./websocket');
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);    //Extrai do express o servidor http
+
+setupWebSocket(server);     //Após instanciar o server, é enviado para a funçõa...
 
 mongoose.connect('mongodb+srv://devradar:devradar@cluster0-z0shr.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
